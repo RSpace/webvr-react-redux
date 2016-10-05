e2a1d14000ff7684459e91d5bc6c278e7993396f
Casper Fabricius <me@casperfabricius.com>
2016-10-05 21:21:18 +0200

    A few more tweaks to get the deploy working.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Copy template files to src folder
+mkdir -p ./src/js
 cp ./_final/entrypoint2.js ./src/js/entrypoint.js
 cp ./_final/panorama2.js ./src/js/panorama.js
 cp ./_final/store.js ./src/js/store.js
@@ -13,3 +14,6 @@ cp ./src/html/production/* ./build/
 
 # Copy images to build folder
 cp -r ./src/images/ ./build/images
+
+# Remove js files from src
+rm -r ./src/js
